ae0da6528c1657296bb769bed0f541ffb70b5046
Jonathan Leitschuh <Jonathan.Leitschuh@gradle.com>
2020-01-13 12:39:40 -0500

    Add a homoglyph detector for gradle-wrapper.jar files

--- __tests__/validate.test.ts
+++ __tests__/validate.test.ts
@@ -4,11 +4,11 @@ import * as validate from '../src/validate'
 test('validates wrapper jars', async () => {
   const invalidWrapperJars = await validate.findInvalidWrapperJars(
     path.resolve('.'),
-    2,
+    3,
     false,
     []
   )
-  expect(invalidWrapperJars.length).toBe(1)
+  expect(invalidWrapperJars.length).toBe(2)
   expect(invalidWrapperJars[0]).toEqual(
     new validate.InvalidWrapperJar(
       '__tests__/data/invalid/gradle-wrapper.jar',
@@ -19,8 +19,8 @@ test('validates wrapper jars', async () => {
 
 test('fails if not enough wrapper jars are found', async () => {
   await expect(
-    validate.findInvalidWrapperJars(path.resolve('.'), 3, false, [])
+    validate.findInvalidWrapperJars(path.resolve('.'), 4, false, [])
   ).rejects.toThrowError(
-    'Expected to find at least 3 Gradle Wrapper JARs but got only 2'
+    'Expected to find at least 4 Gradle Wrapper JARs but got only 3'
   )
 })
--- src/find.ts
+++ src/find.ts
@@ -2,12 +2,17 @@ import * as util from 'util'
 import * as path from 'path'
 import * as fs from 'fs'
 
+import * as homoglyphs from './homoglyphs'
+
 const readdir = util.promisify(fs.readdir)
 
 export async function findWrapperJars(baseDir: string): Promise<string[]> {
+  const targetWords = ['gradle-wrapper.jar']
   const files = await recursivelyListFiles(baseDir)
   return files
-    .filter(file => file.endsWith('gradle-wrapper.jar'))
+    .filter(
+      file => homoglyphs.search(path.basename(file), targetWords).length > 0
+    )
     .map(wrapperJar => path.relative(baseDir, wrapperJar))
 }
 
