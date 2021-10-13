11a69d5d78d2980631ef316e3b5221a200a445e7
Simon Schrottner <s.schrottner@netconomy.net>
2021-10-13 20:59:39 +0200

    Make Wrapper validator executable and GitLab CI ready
    Not all OS Projects are using GitHub and have the benefit of using
    GitHub Actions. With this changes the Wrapper-Validation-Action
    is executable on its own. It will use command line options for
    configuration instead of GitHub Action Inputs.
    
    Additionally a GitLab CI template was added and a Dockerfile for
    building a ready to use Dockerimage.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:12-slim
+
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install && npm run-script build && npm install -g
+
+ENTRYPOINT [ "wrapper-validation-action"]
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
+#!/usr/bin/env node
+
 import * as path from 'path'
 import * as core from '@actions/core'
 
 import * as validate from './validate'
+import { Command } from 'commander';
 
 export async function run(): Promise<void> {
   try {
+    var minWrapperCount
+    var allowSnapshots
+    var allowChecksums
+    if(process.env.GITHUB_ACTION) {
+      minWrapperCount = +core.getInput('min-wrapper-count')
+      allowSnapshots = core.getInput('allow-snapshots')
+      allowChecksums = core.getInput('allow-checksums')
+    } else {
+      const program = new Command()
+      program
+        .description("Gradle Wrapper Validation")
+        .option('-m, --min-wrapper-count <number>', 'Minimum expected wrapper JAR files', '1')
+        .option('-s, --allow-snapshots <boolean>', 'Allow snapshot Gradle versions', 'false')
+        .option('-c, --allow-checksums [checksums...]', 'Allow arbitrary checksums, comma separated', '')
+        
+      program.parse(process.argv)
+      const options = program.opts()
+      minWrapperCount = +options.minWrapperCount
+      allowSnapshots = options.allowSnapshots
+      allowChecksums = options.allowChecksums
+    }
+
     const result = await validate.findInvalidWrapperJars(
       path.resolve('.'),
-      +core.getInput('min-wrapper-count'),
-      core.getInput('allow-snapshots') === 'true',
-      core.getInput('allow-checksums').split(',')
+      minWrapperCount,
+      allowSnapshots === 'true',
+      allowChecksums.split(',')
     )
     if (result.isValid()) {
       core.info(result.toDisplayString())
