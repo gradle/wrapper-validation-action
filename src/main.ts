91993932b6c02b88ab8124285d23ba7807721a92
Simon Schrottner <s.schrottner@netconomy.net>
2021-10-14 09:19:17 +0200

    removing redundant type specifier

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,12 @@
 #!/usr/bin/env node
 
-import * as path from 'path'
-import * as fs from 'fs'
-import * as core from '@actions/core'
+import path from 'path'
+import { promises as fs } from 'fs'
+import core from '@actions/core'
 
-import * as validate from './validate'
+import { findInvalidWrapperJars } from './validate'
 import { Command } from 'commander'
-import * as jsyaml from 'js-yaml'
-
-export async function run(): Promise<void> {
-  try {
-    let minWrapperCount
-    let allowSnapshots
-    let allowChecksums
-    if (process.env.GITHUB_ACTION) {
-      minWrapperCount = +core.getInput('min-wrapper-count')
-      allowSnapshots = core.getInput('allow-snapshots')
-      allowChecksums = core.getInput('allow-checksums')
-    } else {
-      const program = new Command()
-      const actionYaml: Action = jsyaml.load(await fs.promises.readFile(path.resolve('action.yml'), 'utf8')) as Action
-
-      program
-        .description(actionYaml.description)
-
-      for (const [key, value] of Object.entries(actionYaml.inputs)) {
-        program.option('--' + key + ' <value>', value.description, value.default)
-      }
-
-      program.parse(process.argv)
-      const options = program.opts()
-      minWrapperCount = +options.minWrapperCount
-      allowSnapshots = options.allowSnapshots
-      allowChecksums = options.allowChecksums
-    }
-
-    const result = await validate.findInvalidWrapperJars(
-      path.resolve('.'),
-      minWrapperCount,
-      allowSnapshots === 'true',
-      allowChecksums.split(',')
-    )
-    if (result.isValid()) {
-      core.info(result.toDisplayString())
-    } else {
-      core.setFailed(
-        `Gradle Wrapper Validation Failed!\n  See https://github.com/gradle/wrapper-validation-action#reporting-failures\n${result.toDisplayString()}`
-      )
-    }
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
-
-run()
+import jsyaml from 'js-yaml'
 
 interface Action {
   name: string;
@@ -67,3 +20,55 @@ interface Input {
   required?: boolean;
   default?: string; 
 }
+
+export async function run(): Promise<void> {
+  try {
+    let minWrapperCount: number
+    let allowSnapshots: string
+    let allowChecksums: string
+    
+    if (process.env.GITHUB_ACTION) {
+      minWrapperCount = +core.getInput('min-wrapper-count')
+      allowSnapshots = core.getInput('allow-snapshots')
+      allowChecksums = core.getInput('allow-checksums')
+
+    } else {
+      const program = new Command()
+      const actionYaml = jsyaml.load(await fs.readFile(path.resolve('action.yml'), 'utf8')) as Action
+
+      program
+        .description(actionYaml.description)
+        .name('wrapper-validation')
+      
+      Object.entries(actionYaml.inputs).forEach(([key,value]) => {
+          program.option(`--${key} <value>`, value.description, value.default)
+      })
+
+      program.parse(process.argv)
+      const options = program.opts()
+      minWrapperCount = +options.minWrapperCount
+      allowSnapshots = options.allowSnapshots
+      allowChecksums = options.allowChecksums
+    }
+
+    const result = await findInvalidWrapperJars(
+      path.resolve('.'),
+      minWrapperCount,
+      allowSnapshots === 'true',
+      // --allow-snapshots , --allow-snapshots false/true
+      allowChecksums.split(',')
+    )
+    if (result.isValid()) {
+      core.info(result.toDisplayString())
+    } else {
+      core.setFailed(
+        `Gradle Wrapper Validation Failed!\n  See https://github.com/gradle/wrapper-validation-action#reporting-failures\n${result.toDisplayString()}`
+      )
+    }
+  } catch (error) {
+    if(error instanceof Error)
+      core.setFailed(error.message)
+  }
+}
+
+run()
