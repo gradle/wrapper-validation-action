9efc350e9fc73ba19c05826d888a44d32326d7cb
Marcono1234 <Marcono1234@users.noreply.github.com>
2024-02-03 21:38:01 +0100

    Hardcoded checksums follow-up

--- __tests__/checksums.test.ts
+++ __tests__/checksums.test.ts
@@ -4,9 +4,32 @@ import {afterEach, describe, expect, test, jest} from '@jest/globals'
 
 jest.setTimeout(30000)
 
+test('has loaded hardcoded wrapper jars checksums', async () => {
+  // Sanity check that generated checksums file is not empty and was properly imported
+  expect(checksums.KNOWN_VALID_CHECKSUMS.size).toBeGreaterThan(10)
+  // Verify that checksums of arbitrary versions are contained
+  expect(
+    checksums.KNOWN_VALID_CHECKSUMS.get(
+      '660ab018b8e319e9ae779fdb1b7ac47d0321bde953bf0eb4545f14952cfdcaa3'
+    )
+  ).toEqual(new Set(['4.10.3']))
+  expect(
+    checksums.KNOWN_VALID_CHECKSUMS.get(
+      '28b330c20a9a73881dfe9702df78d4d78bf72368e8906c70080ab6932462fe9e'
+    )
+  ).toEqual(new Set(['6.0-rc-1', '6.0-rc-2', '6.0-rc-3', '6.0', '6.0.1']))
+})
+
 test('fetches wrapper jars checksums', async () => {
   const validChecksums = await checksums.fetchValidChecksums(false)
   expect(validChecksums.size).toBeGreaterThan(10)
+  // Verify that checksum of arbitrary version is contained
+  expect(
+    validChecksums.has(
+      // Checksum for version 6.0
+      '28b330c20a9a73881dfe9702df78d4d78bf72368e8906c70080ab6932462fe9e'
+    )
+  ).toBe(true)
 })
 
 describe('retry', () => {
