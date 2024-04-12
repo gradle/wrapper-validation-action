506ef10523a327f81e20f0ab26323daf3a68af4b
daz <daz@gradle.com>
2024-04-12 14:59:50 -0600

    Delegate to gradle/actions/wrapper-validation

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts', 'json'],
-  testMatch: ['**/*.test.ts'],
-  preset: 'ts-jest',
-  verbose: true,
-  setupFilesAfterEnv: ['./jest.setup.js']
-}
